8c3eb0383edeb9122380e17b944cc949e08b4c41
aranganath24 <aranganath24@gmail.com>
2021-08-21 22:13:44 -0400

    joins data

--- icpsr_code.R
+++ icpsr_code.R
@@ -20,3 +20,47 @@ worldmap<-tm_shape(world)+
            tm_polygons()
 
 
+## Prepare covid data for mapping
+
+covid_data_economic<-covid_data %>% select(iso, Economic_Measures) 
+covid_data_economic$Economic_Measures<-as.numeric(covid_data_economic$Economic_Measures)
+class(covid_data_economic$Economic_Measures)
+
+covid_data_economic<-covid_data_economic %>% group_by(iso) %>% 
+                                             summarize(mean_economic=mean(Economic_Measures, na.rm=FALSE))
+
+
+## Join Covid data to world boundaries data
+
+worldmap_covid_data_economic<-full_join(world, covid_data_economic, by=c("iso_a3"="iso"))
+
+
+
+
+
+
+
+
+
+
+
+
+##Introduction (ICPSR, motivation)
+##Download data from ICPSR
+## Load ICPSR data into R Studio
+## Bring in map data and visualize map
+##Clean ICPSR data
+## Join ICPSR data to world map
+## Create map
+## Customize
+## Conclusion
+## Student Activity
+## Answers to student activity 
+## Student challenge
+## Answers to student challenge
+
+
+
+
+
+https://r-spatial.org/r/2018/10/25/ggplot2-sf.html
